43d23f8465232182ef95eb4587386aed93847de7
wangkai <wk15@mail.ustc.edu.cn>
2021-09-13 01:29:54 +0800

    20210913 | wangkai

--- 260.只出现一次的数字-iii.py
+++ 260.只出现一次的数字-iii.py
@@ -8,6 +8,24 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        if n<=2:
+            return nums
+        result = []
+        temp = nums[0]
+        for i in range(1,n):
+            temp = temp^nums[i]
+        
+        # 找从最低位第一个不为0的
+        div = 1
+        while div&temp==0:
+            div = div<<1
+        a,b =0,0
+        for value in nums:
+            if div&value!=0:
+                a = a^value
+            else:
+                b = b^value
+        return [a,b]
         
 # @lc code=end
 
